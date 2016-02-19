8e4ce7b8263b7168f5c203a68bdd04a5014acf3e
Jesús Flores <jesusfloressanjose@gmail.com>
2016-02-19 16:05:01 +0100

    bug in name

--- src/AppBundle/Command/FogonazosCommand.php
+++ src/AppBundle/Command/FogonazosCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class XatakaCommand extends ContainerAwareCommand
+class FogonazosCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
